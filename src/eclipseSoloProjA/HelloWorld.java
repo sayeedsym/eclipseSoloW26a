26132bdcecd0d42e2bc4f04c7205450ce876f522
sayeedsym <asayeed@emich.edu>
2026-02-14 16:56:46 -0500

    programmer2 made changes without pulling past work

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,5 +5,5 @@ public class HelloWorld {
 			System.out.println("Howdy github project");
 			System.out.println("This is a new println command");
 			System.out.println("this is done by programmer 2");
-		}
+			System.out.println("Changes by programmer 2 without pulling first");		}
 }
