11e0e54b0157d1cfa7b3c822fde1164ab5e6fd81
sayeedsym <asayeed@emich.edu>
2026-02-14 16:47:11 -0500

    programmer 2 had made some changes on line 7

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 		public static void main(String [] args) {
 			System.out.println("Howdy github project");
 			System.out.println("This is a new println command");
+			System.out.println("this is done by programmer 2");
 		}
 }
